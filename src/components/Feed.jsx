d3058f576d7de610c2618de1fe2002c36f601e22
Jinho Jang <verycosy@kakao.com>
2023-03-01 18:01:23 +0900

    스켈레톤

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,18 +1,36 @@
-import { Box } from '@mui/material';
+import { Box, Skeleton, Stack } from '@mui/material';
+import { useState } from 'react';
 import { Post } from './Post';
 
 export const Feed = () => {
+  const [loading, setLoading] = useState(true);
+
+  setTimeout(() => {
+    setLoading(false);
+  }, [3000]);
+
   return (
     <Box flex={4} p={2}>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {loading ? (
+        <Stack>
+          <Skeleton variant='text' height={100} />
+          <Skeleton variant='text' height={20} />
+          <Skeleton variant='text' height={20} />
+          <Skeleton variant='rectangular' height={300} />
+        </Stack>
+      ) : (
+        <>
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </>
+      )}
     </Box>
   );
 };
